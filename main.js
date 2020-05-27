9e0a678dd8d8251c79818606093afa2bef605972
Jonathan Forney <jonathan.forney@gmail.com>
2020-05-26 23:58:19 -0700

    notes about data structure

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+
+
+
+// UPDATE table_name
+// SET column1 = value1, column2 = value2...., columnN = valueN.
+// WHERE [condition];
+
+
+
+
 /* Episodes
 //  number
 //  air_date
@@ -20,9 +30,10 @@ EPISODES:
 id(PK) / episodeName / synopsis / airDate / Season
 
 QUOTES:
-id(PK) / Text / characterId(FK) / episodeId(FK) /
-             or
-id(PK) / Text             <--- Trying this way first
+id(PK) / Text /  episodeId(FK)  <-- episodes tracked here in quotes because each quote
+//                                   can only come from one episode, but characters stored
+//                                 elsewhere because a quote can be bit of dialoge between
+//                                 more than one character.
 
 CHARACTERS_TO_QUOTES:
 id(PK) / characterId(FK) / quoteId(FK) / *any other columns which are related to this combo.
