9ab9a42929cea9670d84f001fe6212986ab89a34
Jonathan Forney <jonathan.forney@gmail.com>
2020-06-01 17:22:24 -0700

    conected to postgres db! added most characters routes

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const app = express();
 const logger = require('morgan');
 const port = process.env.PORT || 3000;
 const path = require('path');
+const db = require('./queries');
 
 app.use(bodyParser.json());
 app.use(logger('dev'));
@@ -17,13 +18,20 @@ app.get('/json', (req, res) => {
   res.json({ info: 'Node.js, Express, and Postgres API' });
 });
 
+app.get('/characters', db.getCharacters);
+app.get('/characters/:id', db.getCharactersById);
+app.post('/characters', db.createCharacter);
+app.delete('/characters/:id', db.deleteCharacter);
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.get('/godzilla.jpg', (req, res) => {
-  res.sendFile(path.join(__dirname + '/godsilla.jpg'));
-});
+
+
+// app.get('/godzilla.jpg', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/godsilla.jpg'));
+// });
 
 
 app.listen(port, () => {
--- queries.js
+++ queries.js
@@ -3,7 +3,59 @@ const Pool = require('pg').Pool;
 const pool = new Pool({
   user: 'jon',
   host: 'localhost',
-  database: 'code-bros',
+  database: 'code_bros',
   password: 'password',
   port: 5432,
 });
+
+const getCharacters = (req, res) => {
+  pool.query('SELECT * FROM characters ORDER BY id ASC', (error, results) => {
+    if(error) {
+      throw error;
+    }
+    res.status(200).json(results.rows);
+  });
+};
+
+const getCharactersById = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+    if(error) {
+      throw error;
+    }
+    res.status(200).json(results.rows);
+  });
+};
+
+const createCharacter = (req, res) => {
+  const { name, actor, job, imgpath } = req.body;
+
+  pool.query('INSERT INTO  characters (name, actor, job, imgpath) VALUES ($1, $2, $3, $4)',
+    [name, actor, job, imgpath], (error, result) => {
+      if(error) {
+        throw error;
+      }
+      res.status(201).send(
+        `Character ${req.name} has been added (POSTED) to the database with Character ID: ${res.insertId}`
+      );
+    });
+};
+
+const deleteCharacter = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  pool.query('DELETE FROM characters WHERE id = $1', [id], (error, result) => {
+    if(error) {
+      throw error;
+    }
+    res.status(200).send(`Character with ID: ${id} has been DELETED`);
+  });
+};
+
+module.exports = {
+  getCharacters,
+  getCharactersById,
+  createCharacter,
+  deleteCharacter,
+};
