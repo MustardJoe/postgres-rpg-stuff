bc387519fca04d8e810690dfe987eb94e9547bad
Jonathan Forney <jonathan.forney@gmail.com>
2020-07-01 22:02:08 -0700

    did some practice work

---
+++ oldWork/firstUnique.js
@@ -0,0 +1,24 @@
+const data1 = "ppaathrise";
+// return 4.
+
+function firstUniqChar(s) {
+  let charArr = s.split('');
+  let charCount = {};
+  let charSet = [...new Set(charArr)];
+  for(let i = 0; i < charSet.length; i++) {
+    charCount[charSet[i]] = 0;
+  }
+  for(let j = 0; j < charArr.length; j++) {
+    charCount[s.split('')[j]]++;
+  }
+  let appearsOnlyOnce = []
+  for(let k = 0; k < charArr.length; k++) {
+
+    if(charCount[charSet[k]] && charCount[charSet[k]] < 2) {
+      appearsOnlyOnce.push(Object.keys(charCount)[k]);
+    }
+  }
+  return charArr.indexOf(appearsOnlyOnce[0]);
+}
+
+firstUniqChar(data1);
