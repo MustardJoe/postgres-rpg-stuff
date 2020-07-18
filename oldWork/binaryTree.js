ec4bafc06775da922db7d6f86a4f9be6676fc68a
Jonathan Forney <jonathan.forney@gmail.com>
2020-07-17 18:00:14 -0700

    trying to add hasNode to my tree class

--- oldWork/binaryTree.js
+++ oldWork/binaryTree.js
@@ -37,7 +37,9 @@ class BinaryTree {
   constructor(rootNode) {
     this.rootNode = rootNode;
     this.addNode = this.addNode.bind(this);
+    this.hasNode = this.hasNode.bind(this);
   }
+
   addNode(node, parentNode = this.rootNode) {
     if(node.data >= parentNode.data) {
       if(!parentNode.right) {
@@ -55,8 +57,31 @@ class BinaryTree {
       else {
         return this.addNode(node, parentNode.left);
       }
-    } 
-    
+    }  
+  }
+
+  hasNode(possibleNode, comparitorNode = this.rootNode) {
+    let result = false;
+    /* eslint-disable-next-line no-console */
+    console.log('hasNode running', possibleNode.data, comparitorNode.data, possibleNode.left, comparitorNode.left,
+      possibleNode.right, comparitorNode.right);
+    if(possibleNode.data === comparitorNode.data 
+      && possibleNode.left === comparitorNode.left
+      && possibleNode.right === comparitorNode.right) {
+      result = (possibleNode.data === comparitorNode.data 
+        && possibleNode.left === comparitorNode.left
+        && possibleNode.right === comparitorNode.right);
+    }
+    else if(comparitorNode.left) {
+      // comparitorNode = comparitorNode.left;
+      this.hasNode(possibleNode, comparitorNode.left);
+    }
+    else if(comparitorNode.right) {
+      // comparitorNode = comparitorNode.right;
+      this.hasNode(possibleNode, comparitorNode.right);
+    }
+    console.log('result', result);
+    return result;
   }
 }
 
@@ -64,14 +89,25 @@ let node1 = new Node(5);
 let node2 = new Node(3);
 let node3 = new Node(8);
 let node4 = new Node(4);
+// console.log('node2', node2);
 
 let ourTree = new BinaryTree(node1);
 
 ourTree.addNode(node2);
-console.log(node1.data);
+// console.log(node1.data);
 ourTree.addNode(node3);
-console.log(ourTree.rootNode);
+// console.log(ourTree.rootNode);
 ourTree.addNode(node4);
-console.log(ourTree.rootNode);
+/* eslint-disable-next-line no-console */
+// console.log('Tree, after node insterts', ourTree.rootNode);
+/* eslint-disable-next-line no-console */
+// console.log(`
+//   -------------------------------------
+//   console.logging node2: ${node2}
+// `);
+// console.log('dofferent node 2, but actually the same', node2);
+ourTree.hasNode(node2);
+
+
 
 
