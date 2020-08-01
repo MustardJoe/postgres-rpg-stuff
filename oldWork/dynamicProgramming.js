9b40c0950828e729c7498e803047c47f47243b53
Jonathan Forney <jonathan.forney@gmail.com>
2020-07-31 17:31:50 -0700

    LL DP change finding algo problem

--- oldWork/dynamicProgramming.js
+++ oldWork/dynamicProgramming.js
@@ -9,35 +9,40 @@
 
 let ourArr = [1, 5, 10];
 
-const changeFinder = (arr, amount) => {
-  let returnArr = [];
-
-  if(amount >= arr[arr.length - 1]) {
-    console.log('need big coin');
-    returnArr.push(arr[arr.length - 1]);
-    let currentCoins = Math.floor(amount / arr[arr.length - 1]);
-    console.log('current coins thing', currentCoins);
-    for(let i = 1)
+const changeFinder = (arr, amount, returnArr = []) => {
+  // Initial check to see if we have valid inputs and need to run our function
+  if(amount <= 0 && returnArr.length < 1) {
+    /* eslint-disable-next-line no-console */
+    console.log('No change needed');
+    return;
   }
-  console.log('amount modulo biggest coin', amount % arr[arr.length - 1], 'amount divided by big coin', Math.floor(amount / arr[arr.length - 1]));
-  //check to see if we need the biggest coin, if we do, check to see how
-  //many of them we need
 
-  // check are we finished or is there remaining change to divy up
+  // Recursive 'End' guard clause: check are we finished (remaining change to divy up is 0 and returnArr isn't empty)
+  if(amount === 0 && returnArr.length >= 1) {
+    /* eslint-disable-next-line no-console */
+    console.log('Final Change To Return:', returnArr.sort((a, b) => {
+      return a - b;
+    }));
+    return returnArr.sort((a, b) => {
+      return a - b;
+    });
+  }
 
-  // recursively call the changeFinder function, passing in a modified array
-  // without the larget coin and also passing in the remaining amount after 
-  // subtracting the largest coins
+  //check to see if we need the biggest coin: if we do, check to see how many of them we need
+  // then subtract from the 'amount' the value of the big coin/s
+  if(amount >= arr[arr.length - 1]) {
+    let numbNeededOfCurentCoin = Math.floor(amount / arr[arr.length - 1]);
+    for(let i = 1; i <= numbNeededOfCurentCoin; i++) {
+      returnArr.push(arr[arr.length - 1]);
+      amount = amount - arr[arr.length - 1];
+    }
+  }
 
-  // return [returnValue, ...changeFinder(subSet, remainingCoins)]
+  // whether we need the big coin or not, when we are done checking for it, we remove it from array
+  arr.pop(arr.length - 1);
 
-
-
-
-  // if(arr[arr.length - 1] <= amount) {
-
-  // }
+  // run the recursive call with the remaining amount and remaing coins to be checked in arr
+  return changeFinder(arr, amount, returnArr);
 };
 
-changeFinder(ourArr, 23);
-
+changeFinder(ourArr, 28);
