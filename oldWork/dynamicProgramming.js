2e4a8d8f8d8189d00b68db1f606d3ebd3c5f703d
Jonathan Forney <jonathan.forney@gmail.com>
2020-08-04 15:54:51 -0700

    small tweaks to change finder memoization algo

--- oldWork/dynamicProgramming.js
+++ oldWork/dynamicProgramming.js
@@ -7,42 +7,38 @@
 // it will return an Array with the least number of coins possible 
 // like so [1, 7, 10]
 
-let ourArr = [1, 5, 10];
+let ourArr = [3, 7, 10];
 
-const changeFinder = (arr, amount, returnArr = []) => {
+const changeFinder = (coinDenominations, amount, changeGiven = []) => {
   // Initial check to see if we have valid inputs and need to run our function
-  if(amount <= 0 && returnArr.length < 1) {
+  if(amount <= 0 && changeGiven.length < 1) {
     /* eslint-disable-next-line no-console */
     console.log('No change needed');
     return;
   }
 
   // Recursive 'End' guard clause: check are we finished (remaining change to divy up is 0 and returnArr isn't empty)
-  if(amount === 0 && returnArr.length >= 1) {
+  if(amount < Math.min(...coinDenominations) && changeGiven.length >= 1) {
     /* eslint-disable-next-line no-console */
-    console.log('Final Change To Return:', returnArr.sort((a, b) => {
-      return a - b;
-    }));
-    return returnArr.sort((a, b) => {
-      return a - b;
-    });
+    console.log('Final Change To Return:', changeGiven);
+    return changeGiven;
   }
 
   //check to see if we need the biggest coin: if we do, check to see how many of them we need
   // then subtract from the 'amount' the value of the big coin/s
-  if(amount >= arr[arr.length - 1]) {
-    let numbNeededOfCurentCoin = Math.floor(amount / arr[arr.length - 1]);
+  if(amount >= coinDenominations[coinDenominations.length - 1]) {
+    let numbNeededOfCurentCoin = Math.floor(amount / coinDenominations[coinDenominations.length - 1]);
     for(let i = 1; i <= numbNeededOfCurentCoin; i++) {
-      returnArr.push(arr[arr.length - 1]);
-      amount = amount - arr[arr.length - 1];
+      changeGiven.unshift(coinDenominations[coinDenominations.length - 1]);
+      amount = amount - coinDenominations[coinDenominations.length - 1];
     }
   }
 
   // whether we need the big coin or not, when we are done checking for it, we remove it from array
-  arr.pop(arr.length - 1);
-
-  // run the recursive call with the remaining amount and remaing coins to be checked in arr
-  return changeFinder(arr, amount, returnArr);
+  coinDenominations.pop(coinDenominations.length - 1);
+  console.log(coinDenominations, amount, changeGiven);
+  // run the recursive call with the remaining amount and remaing coins to be checked in coinDenominations
+  return changeFinder(coinDenominations, amount, changeGiven);
 };
 
 changeFinder(ourArr, 28);
